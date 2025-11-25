323173e2d6607a395263130024a862f00013901d
Dennis <johndoe@example.com>
2025-11-25 23:10:24 +0100

    added viewport component and three package

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,21 @@
 import { useState } from 'react'
 import './App.css'
 
-
 import CornerLogo from './components/CornerLogo.tsx'
+import Viewport from "./components/Viewport.tsx";
 
 function App() {
 
   return (
     <>
-      <div>
-      <CornerLogo />
+      <div className="app-layout">
+        <aside className="app-sidebar">
+          <h1>Digital Twin</h1>
+            <CornerLogo />
+        </aside>
+        <main className="app-main">
+            <Viewport />
+        </main>
       </div>
     </>
   )
