520cafd9ce364e32f5d048c6bf40decec420af83
Dennis <johndoe@example.com>
2025-12-03 16:14:52 +0100

    added heroUI to project

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import "./global.css";
+import { HeroUIProvider } from "@heroui/react";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <HeroUIProvider>
+      <App />
+    </HeroUIProvider>
   </StrictMode>,
-)
+);
