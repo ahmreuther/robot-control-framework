5dde43e8c3744e106e23302d038f7b59a71a8d9d
Niklas Bönisch <boenisch@plcm.tu-darmstadt.de>
2026-06-11 12:19:13 +0200

    Fix hosting bug

--- backend/src/backend/app.py
+++ backend/src/backend/app.py
@@ -27,4 +27,4 @@ app = create_app()
 def main() -> None:
     import uvicorn
 
-    uvicorn.run("backend.app:app", host="127.0.0.1", port=8000)
+    uvicorn.run("backend.app:app", host="0.0.0.0", port=8000)
